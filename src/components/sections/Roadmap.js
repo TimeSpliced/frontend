395f5314ffd66987502f0f1a94c1bacaa36b7419
Rachel <rachblondon@gmail.com>
2020-09-06 00:52:41 +0200

    roadmap

--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -45,7 +45,7 @@ class Roadmap extends React.Component {
 
     const sectionHeader = {
       title: 'Product roadmap',
-      paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      paragraph: 'Our journey revinventing savings for the DeFi generation.'
     };
 
     return (
@@ -57,21 +57,21 @@ class Roadmap extends React.Component {
           <div className={innerClasses}>
             <SectionHeader data={sectionHeader} className="center-content" />
             <Timeline>
-              <TimelineItem title="November 2019">
-                Deployed a high-quality first release and conducted a market validation test
-              </TimelineItem>
-              <TimelineItem title="December 2019">
-                Deployed a high-quality first release and conducted a market validation test
-              </TimelineItem>
-              <TimelineItem title="January 2020">
-                Deployed a high-quality first release and conducted a market validation test
-              </TimelineItem>
               <TimelineItem title="February 2020">
-                Deployed a high-quality first release and conducted a market validation test
+                Initial product ideation at ETH London, award a finalist position.
               </TimelineItem>
-              <TimelineItem title="March 2020">
-                Deployed a high-quality first release and conducted a market validation test
+              <TimelineItem title="July 2020">
+                GoodGhosting joins Gitcoin's Kernel program. New team formation.
               </TimelineItem>
+              <TimelineItem title="August 2020">
+                Concept launch and validiated.
+              </TimelineItem>
+              <TimelineItem title="Q4 2020">
+                Mainnet launch with gas minimised MVP architecture.
+              </TimelineItem>
+              {/* <TimelineItem title="March 2020">
+                Deployed a high-quality first release and conducted a market validation test
+              </TimelineItem> */}
             </Timeline>
           </div>
         </div>
