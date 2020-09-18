b05ae776e4e7bbcf03937adcd3948df2e9d4fb37
Rachel <rachblondon@gmail.com>
2020-09-18 14:41:38 +0200

    new address

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -10,7 +10,7 @@ export const status = {
 export const isNotEmptyObj = (myObject) => (!!Object.keys(myObject).length);
 
 export const daiAddress = "0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD";
-export const goodGhostingAdress ="0x3945981bc19A02b6D4eF43a667ecD2Bea70B3364" // 1 day
+export const goodGhostingAdress ="0xFA369CaDF6e982bF0E0b81dc84A78F98Cd7Ec87a" // 1 day
 // export const goodGhostingAdress = '0x79C01423De3Ca5c436dF4996c9B16d796c871370' //1800 secs
 // export const goodGhostingAdress = "0x839f2F25216621D01D0567646c459d960abD6267";
 
