63c6a8f58ba0048b3725d3ea529b03c9196fe528
Deepcryptodive <61325205+Deepcryptodive@users.noreply.github.com>
2020-11-06 14:54:01 +0100

    Update utilities.js

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -10,7 +10,7 @@ export const status = {
 export const isNotEmptyObj = (myObject) => !!Object.keys(myObject).length;
 
 export const daiAddress = "0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD";
-export const goodGhostingAdress = "0xfef637ea4c922409e8308834d84718bbd2a200f6"; // 1 day
+export const goodGhostingAdress = "0x7c820f2679bdac26be00c68be8c19133a09df6ca"; // short demo game
 // export const goodGhostingAdress = '0x79C01423De3Ca5c436dF4996c9B16d796c871370' //1800 secs
 // export const goodGhostingAdress = "0x839f2F25216621D01D0567646c459d960abD6267";
 
