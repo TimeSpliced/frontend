177b8342c17ff54999b36b13252a6d456607f65f
Deepcryptodive <61325205+Deepcryptodive@users.noreply.github.com>
2020-12-16 11:38:00 +0100

    Roadmap update + fixed typo + removed punctuation
    Roadmap update + fixed typo + removed punctuation

--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
-import SectionHeader from './partials/SectionHeader';
-import Timeline from '../elements/Timeline';
-import TimelineItem from '../elements/TimelineItem';
+import React from "react";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Timeline from "../elements/Timeline";
+import TimelineItem from "../elements/TimelineItem";
 
 const propTypes = {
-  ...SectionProps.types
-}
+  ...SectionProps.types,
+};
 
 const defaultProps = {
-  ...SectionProps.defaults
-}
+  ...SectionProps.defaults,
+};
 
 class Roadmap extends React.Component {
-
   render() {
-
     const {
       className,
       topOuterDivider,
@@ -29,53 +27,86 @@ class Roadmap extends React.Component {
     } = this.props;
 
     const outerClasses = classNames(
-      'roadmap section',
-      topOuterDivider && 'has-top-divider',
-      bottomOuterDivider && 'has-bottom-divider',
-      hasBgColor && 'has-bg-color',
-      invertColor && 'invert-color',
+      "roadmap section",
+      topOuterDivider && "has-top-divider",
+      bottomOuterDivider && "has-bottom-divider",
+      hasBgColor && "has-bg-color",
+      invertColor && "invert-color",
       className
     );
 
     const innerClasses = classNames(
-      'roadmap-inner section-inner',
-      topDivider && 'has-top-divider',
-      bottomDivider && 'has-bottom-divider'
+      "roadmap-inner section-inner",
+      topDivider && "has-top-divider",
+      bottomDivider && "has-bottom-divider"
     );
 
     const sectionHeader = {
-      title: 'Product roadmap',
-      paragraph: 'Our journey revinventing savings for the DeFi generation.'
+      title: "Product roadmap",
+      paragraph: "Our journey revinventing savings for the DeFi generation.",
     };
 
     return (
-      <section
-        {...props}
-        className={outerClasses}
-      >
+      <section {...props} className={outerClasses}>
         <div className="container" id="roadmap">
           <div className={innerClasses}>
             <SectionHeader data={sectionHeader} className="center-content" />
             <Timeline>
-              <TimelineItem title="February 2020">
-                <a href="https://www.youtube.com/watch?v=nUW8ASimbH8" target="_blank" rel="noopener">
-                Initial product ideation at ETH London, award a finalist position.
+              <TimelineItem title="February 2020" fadeInGG>
+                <a
+                  href="https://www.youtube.com/watch?v=nUW8ASimbH8"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Initial product ideation at ETH London, award a finalist
+                  position
                 </a>
               </TimelineItem>
-              <TimelineItem title="July 2020">
-                GoodGhosting joins Gitcoin's Kernel program. New team formation.
+              <TimelineItem title="July 2020" fadeInGG>
+                GoodGhosting joins Gitcoin's KERNEL program. New team formation
               </TimelineItem>
-              <TimelineItem title="August 2020">
-                <a href="https://medium.com/goodghosting/confession-time-whats-next-f5ac52aa517d" target="_blank"  rel="noopener">Concept launch and validiated.</a>
+              <TimelineItem title="August 2020" fadeInGG>
+                <a
+                  href="https://medium.com/goodghosting/confession-time-whats-next-f5ac52aa517d"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Concept launch and validation
+                </a>
               </TimelineItem>
-              <TimelineItem title="September 2020">
-                <a href="https://gitcoin.co/grants/1112/goodghosting-a-defi-savings-game" target="_blank" rel="noopener"> Our gitcoin grant is live.</a>
+              <TimelineItem title="September 2020" fadeInGG>
+                <a
+                  href="https://gitcoin.co/grants/1112/goodghosting-a-defi-savings-game"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  {" "}
+                  Our gitcoin grant is live
+                </a>
               </TimelineItem>
-              <TimelineItem title="Q4 2020">
-                Testnet launch and UX testing.
+              <TimelineItem title="NOVEMBER 2020" fadeInGG>
+              <a
+                href="https://medium.com/goodghosting/the-beginners-guide-to-using-goodghosting-on-kovan-testnet-9b7edb8fb640"
+                target="_blank"
+                rel="noopener"
+              >
+                Testnet launch</a>
+                <a
+                  href="https://medium.com/goodghosting/goodghosting-alpha-testing-94e1f8c083e1"
+                  target="_blank"
+                  rel="noopener"
+                > & UX testing</a>
               </TimelineItem>
-              <TimelineItem title="Q1 2021">
-                Mainnet launch with gas minimised MVP architecture.
+              <TimelineItem title="December 2020" fadeInGG>
+              <a
+                href="https://twitter.com/metagammadelta/status/1338532174610407424"
+                target="_blank"
+                rel="noopener"
+              >
+                Awarded MakerDAO grant by MGD</a>
+              </TimelineItem>
+              <TimelineItem title="Q1 2021" fadeInGG>
+                Mainnet MVP launch with gas minimized architecture
               </TimelineItem>
               {/* <TimelineItem title="March 2020">
                 Deployed a high-quality first release and conducted a market validation test
